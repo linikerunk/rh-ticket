d3f96ce391e57480be4622ec8313092734185264
linikerj <linikerenem@gmail.com>
2020-05-12 12:17:26 -0300

    erros att

--- chamados/views.py
+++ chamados/views.py
@@ -48,7 +48,7 @@ def enviar(request):
         print("\nArquivo separado : ", files)
         print("\ncampos : ", form.fields)
         print("\ndatas : ", form.data)
-        print("\nerrors : ", form.errors)
+        print("\nerrors : ", form.error)
         if form.is_valid():
             form.save()
             save_it = form.save()
