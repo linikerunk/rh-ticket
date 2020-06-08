75c4e64c61bf87b7654d1429fdbf98c52dade072
linikerj <linikerenem@gmail.com>
2020-06-08 17:03:00 -0300

    arquivos staticos

--- Tickets/settings.py
+++ Tickets/settings.py
@@ -157,9 +157,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'templates/static')
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
