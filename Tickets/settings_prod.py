3c9cc5f57da6c24e12ec7ee964e16f001e6bbd3b
linikerj <linikerenem@gmail.com>
2020-06-23 13:50:01 -0300

    enviando para o github

--- Tickets/settings_prod.py
+++ Tickets/settings_prod.py
@@ -27,11 +27,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY') # enviando para o servidor
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool) 
 
 ALLOWED_HOSTS = ['10.218.24.22', 'rhticket.gs.conti', 'rhticket', 'sltm202x', 'sltm203x', '10.218.23.31', 'centralrh.conti.de']
 
