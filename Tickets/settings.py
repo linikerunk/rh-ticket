3224e5f999b820ff592214811181efbc8550fe06
linikerj <linikerenem@gmail.com>
2020-07-16 12:21:47 -0300

    Testando o  static

--- Tickets/settings.py
+++ Tickets/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
