333bffbe47af8d0ad516328ad1f5ed352c9215db
linikerj <linikerenem@gmail.com>
2020-08-03 17:06:43 -0300

    Indo embora

--- perfil/models.py
+++ perfil/models.py
@@ -59,8 +59,7 @@ class Funcionario(models.Model):
     espelho = models.ImageField(max_length=200, upload_to="espelhos",  default="0000.jpg", verbose_name='Fotos Funcionários')
     
     def get_photo_url(self):
-        # path = f'{PHOTOS_FOLDER}/{self.usuario}.jpg'
-        path = f'/espelhos/181945.jpg'
+        path = f'{PHOTOS_FOLDER}/{self.usuario}.jpg'
 
         print("usuario : ", self.usuario)
 
