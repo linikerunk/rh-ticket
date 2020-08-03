247d81aaa737254d2703e140e9651c531efa2b44
linikerj <linikerenem@gmail.com>
2020-08-03 16:11:04 -0300

    Enviando para o servidor

--- perfil/models.py
+++ perfil/models.py
@@ -61,7 +61,7 @@ class Funcionario(models.Model):
     def get_photo_url(self):
         path = f'{PHOTOS_FOLDER}/{self.usuario}.jpg'
 
-        print("usuario : ", usuario)
+        print("usuario : ", self.usuario)
 
         print("Path : ", path)
 
