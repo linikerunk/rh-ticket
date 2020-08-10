b4bade2462689ac74e6d9984e27a89dd920d8916
linikerj <linikerenem@gmail.com>
2020-08-10 10:48:07 -0300

    Espelho

--- Tickets/settings.py
+++ Tickets/settings.py
@@ -106,24 +106,24 @@ default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
 'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'RHTicket',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET innodb_strict_mode=1',
-        }
         # 'ENGINE': 'django.db.backends.mysql',
         # 'NAME': 'RHTicket',
-        # 'USER': 'RHTicket',
-        # 'PASSWORD': 'Vitesco1*',
-        # 'HOST': '10.218.24.114',
+        # 'USER': 'root',
+        # 'PASSWORD': 'admin',
+        # 'HOST': 'localhost',
         # 'PORT': '3306',
         # 'OPTIONS': {
         #     'init_command': 'SET innodb_strict_mode=1',
         # }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RHTicket',
+        'USER': 'RHTicket',
+        'PASSWORD': 'Vitesco1*',
+        'HOST': '10.218.24.114',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET innodb_strict_mode=1',
+        }
     }
 }
 
